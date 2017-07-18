dce7086bb0e240f2f92c4b328667233595f63aa8
earthbot <earthbot@earthbot.local>
2017-07-18 12:12:25 +0200

    good enough

--- alarm.sh
+++ alarm.sh
@@ -1,1 +1,20 @@
-(echo "`date`: starting..." && sleep $((20 * 60)) && echo "taking break at: `date`" && say "take a break" ) >> log
+#!/bin/sh
+
+DEFAULTINTERVAL=20
+LOG=worklog.txt
+MESSAGE=""
+
+if [ ! -z "$1" ];
+	then
+		MINUTES=$1
+	else
+		MINUTES=$DEFAULTINTERVAL
+fi
+
+# Redirect STDOUT to log file, use STDERR for user output
+exec >> $LOG
+
+echo "`date`: Starting..."
+sleep $(($MINUTES * 60))
+echo "taking a break at `date`"
+say "Taking a break"
